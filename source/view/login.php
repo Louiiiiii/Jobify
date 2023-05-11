0958b5f1837abcf3109df58e353e16bdb39bc118
Kaiser-exe <david.kaiser03@gmail.com>
2023-05-11 14:36:40 +0200

    added login page

---
+++ source/view/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../style/bulma.css">
+    <link rel="stylesheet" href="../style/login.css">
+</head>
+<body>
+    <div class="card">
+        <p class="title is-2">Login</p>
+        <div class="card-content">
+            <div class="card-content__login">
+                <div class="field">
+                    <p class="control has-icons-left has-icons-right">
+                        <input class="input" type="email" placeholder="Email">
+                        <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                        <i class="fas fa-check"></i>
+                        </span>
+                    </p>
+                </div>
+                <div class="field">
+                    <p class="control has-icons-left">
+                        <input class="input" type="password" placeholder="Password">
+                        <span class="icon is-small is-left">
+                        <i class="fas fa-lock"></i>
+                        </span>
+                    </p>
+                </div>
+                <div class="field">
+                    <a>Forgot password?</a>
+                </div>
+                <div class="field">
+                    <button class="button is-primary">Login</button>
+                </div>
+            </div>
+            <div class="card-content__signup">
+                <div class="signup">Not a member? 
+                    <a>Signup now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
