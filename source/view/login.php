9d66b2481a8982013d862abc2503d0b207a8bc16
Kaiser-exe <david.kaiser03@gmail.com>
2023-05-11 17:59:51 +0200

    added registration page

--- source/view/login.php
+++ source/view/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="../style/bulma.css">
-    <link rel="stylesheet" href="../style/login.css">
+    <link rel="stylesheet" href="../style/login_signup.css">
     <link rel="stylesheet" href="https://bulma.io/vendor/fontawesome-free-5.15.2-web/css/all.min.css">
 </head>
 <body>
