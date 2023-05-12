b451dc1e4d6223245e6b9d67f264b0f95d83f55a
ThaBerndl <berndlstephan@gmail.com>
2023-05-12 11:39:19 +0200

    - fixed insertuser

---
+++ source/shared/classes/User.php
@@ -0,0 +1,64 @@
+<?php
+require_once "DB.php";
+class User
+{
+    public $email;
+    public $password;
+    public $db;
+
+    public function __construct($email, $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+        $this->db = new DB();
+    }
+
+    public static function insertuser($email, $password)
+    {
+        $hashedpw = hash('sha512',$password);
+        $user = new User($email,$hashedpw);
+        $stmt = $user->db->pdo->prepare("INSERT INTO jobify.user (email, passwordhash) VALUES (?,?)");
+        $stmt->bindParam(1, $user->email, PDO::PARAM_STR);
+        $stmt->bindParam(2, $user->password, PDO::PARAM_STR);
+        return($stmt->execute()); //True wenn erfolgreich False wenn fehlgeschlagen
+    }
+
+    /*    public static function getAllFreunde($user_id)
+        {
+            $db = new DB();
+            $stmt = $db->pdo->prepare("SELECT * FROM Freund where user_id = ?");
+            $stmt->bindParam(1,$user_id, PDO::PARAM_INT);
+            $stmt->execute();
+            $cnt = 0;
+            $freunde = array();
+            while($row = $stmt->fetch())
+            {
+                $freunde[$cnt] = new Freund($row['user_id'],$row['freund_id']);
+                $cnt = $cnt+1;
+            }
+            unset($row);
+            return $freunde;
+        }
+
+        public function delFreund()
+        {
+            $stmt = $this->db->pdo->prepare("Delete from Freund where user_id = ? and freund_id = ?");
+            $stmt->bindParam(1,$this->user_id , PDO::PARAM_INT);
+            $stmt->bindParam(2,$this->freund_id , PDO::PARAM_INT);
+            $stmt->execute();
+        }
+
+        public function checkFreund()
+        {
+            $stmt = $this->db->pdo->prepare("select * from Freund where user_id = ? and freund_id = ?");
+            $stmt->bindParam(1, $this->user_id, PDO::PARAM_INT);
+            $stmt->bindParam(2, $this->freund_id, PDO::PARAM_INT);
+            $stmt->execute();
+            while($row = $stmt->fetch())
+            {
+                return true;
+            }
+            return false;
+        }
+    */
+}
