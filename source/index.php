04422ef665d6d6b0597e493c1e548d0bfc4d7fe4
Louiiiiii <leon.oberndorfer@gmail.com>
2023-05-12 09:56:18 +0200

    modified index.php and css for it

--- source/index.php
+++ source/index.php
@@ -4,11 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style/bulma.css">
+    <link rel="stylesheet" href="style/login_signup.css">
+    <link rel="stylesheet" href="https://bulma.io/vendor/fontawesome-free-5.15.2-web/css/all.min.css">
 </head>
 <body>
-    Nothing to see here
-    <button class="button">Test</button>
+    <div>
+        <img src="style/img/jobify_slogan.png">
+        <p class="title is-2">JOBIFY - Your next job calls (replace with logo)</p>
+        <div class="field myIndex" >
+            <button class="button is-primary"><a style="color:white;" href="view/login/signup.php">Register here</a></button>
+        </div>
+        <label class="myIndex">Already got an account? Login here</label>
+        <br>
+        <div class="field myIndex">
+            <button class="button is-primary"><a style="color:white;" href="view/login/login.php">Login</a></button>
+        </div>
+    </div>
 </body>
 </html>
+
+
