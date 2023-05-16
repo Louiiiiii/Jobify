29baeae0420204059ef58c69ffb1dcfb35d98333
Stifi <berndl.stephan@gmail.com>
2023-05-16 09:24:43 +0200

    - added inheritance to existing classes

--- source/shared/classes/User.php
+++ source/shared/classes/User.php
@@ -1,23 +1,21 @@
 <?php
-require_once "DB.php";
-class User
+class User extends DB
 {
     public $email;
     public $password;
-    public $db;
 
     public function __construct($email, $password)
     {
         $this->email = $email;
         $this->password = $password;
-        $this->db = new DB();
+        parent::__construct();
     }
 
     public static function insertuser($email, $password)
     {
         $hashedpw = hash('sha512',$password);
         $user = new User($email,$hashedpw);
-        $stmt = $user->db->pdo->prepare("INSERT INTO jobify.user (email, passwordhash) VALUES (?,?)");
+        $stmt = $user->pdo->prepare("INSERT INTO jobify.user (email, passwordhash) VALUES (?,?)");
         $stmt->bindParam(1, $user->email, PDO::PARAM_STR);
         $stmt->bindParam(2, $user->password, PDO::PARAM_STR);
         return($stmt->execute()); //True wenn erfolgreich False wenn fehlgeschlagen
@@ -27,7 +25,7 @@ class User
     {
         $hashedpw = hash('sha512',$password);
         $user = new User($email,$hashedpw);
-        $stmt = $user->db->pdo->prepare("select count(*) from user where email = ? and passwordhash = ?");
+        $stmt = $user->pdo->prepare("select count(*) from user where email = ? and passwordhash = ?");
         $stmt->bindParam(1, $user->email, PDO::PARAM_STR);
         $stmt->bindParam(2, $user->password, PDO::PARAM_STR);
         $stmt->execute();
@@ -41,7 +39,7 @@ class User
     {
         $pw = null;
         $user = new User($email, $pw);
-        $stmt = $user->db->pdo->prepare("select count(*) AS 'count' from user where email = ?");
+        $stmt = $user->pdo->prepare("select count(*) AS 'count' from user where email = ?");
         $stmt->bindParam(1, $user->email, PDO::PARAM_STR);
         $stmt->execute();
         if($stmt->fetch()[0] >= 1){
