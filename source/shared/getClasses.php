608dc6d9f8e6ea02142aa979b316c0a69ac05561
Louiiiiii <leon.oberndorfer@gmail.com>
2023-05-17 15:31:30 +0200

    changed getclasses by including job.php

--- source/shared/getClasses.php
+++ source/shared/getClasses.php
@@ -3,4 +3,5 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/classes/Applicant.php";
     require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/classes/DB.php";
     require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/classes/Company.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/classes/Job.php";
 ?>
