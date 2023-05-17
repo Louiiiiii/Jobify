e8831075689ab58f788366f142ff6091ff24a5df
Julian <julian.pichler4@gmail.com>
2023-05-17 15:42:44 +0200

    added job.php to get classes

--- source/shared/getClasses.php
+++ source/shared/getClasses.php
@@ -3,4 +3,5 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . "/source/shared/classes/Applicant.php";
     require_once $_SERVER['DOCUMENT_ROOT'] . "/source/shared/classes/DB.php";
     require_once $_SERVER['DOCUMENT_ROOT'] . "/source/shared/classes/Company.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/source/shared/classes/Job.php";
 ?>
