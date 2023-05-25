84539ce92582bb988b5288d8dbcb6d3598fa32c9
Kaiser-exe <david.kaiser03@gmail.com>
2023-05-25 17:14:47 +0200

    changed design and able to toggle editing

--- source/view/applicant/profile.php
+++ source/view/applicant/profile.php
@@ -15,7 +15,7 @@
             <div class="field">
                 <div class="file">
                     <label class="file-label">
-                        <input class="file-input" type="file" name="profilePic">
+                        <input class="file-input disabling" type="file" name="profilePic" disabled>
                         <figure class="image is-128x128">
                             <img class="is-rounded" src="../../style/img/user-icon.png">
                         </figure>
@@ -27,13 +27,13 @@
             <div class="field">
                 <label class="label">Firstname</label>
                 <div class="control">
-                    <input name="firstname" class="input" type="text" placeholder="Firstname" required>
+                    <input name="firstname" class="input disabling" type="text" placeholder="Firstname" required disabled>
                 </div>
             </div>
             <div class="field">
                 <label class="label">Lastname</label>
                 <div class="control">
-                    <input name="lastname" class="input" type="text" placeholder="Lastname" required>
+                    <input name="lastname" class="input disabling" type="text" placeholder="Lastname" required disabled>
                 </div>
             </div>
         </div>
@@ -41,7 +41,7 @@
             <div class="field">
                 <label class="label">Birthday</label>
                 <div class="control">
-                    <input name="birthday" class="input" type="date" required>
+                    <input name="birthday" class="input disabling" type="date" required disabled>
                 </div>
             </div>
             <div class="field"></div>
@@ -50,13 +50,13 @@
             <div class="field">
                 <label class="label">Country</label>
                 <div class="control">
-                    <input name="country" class="input" type="text" placeholder="Country" required>
+                    <input name="country" class="input disabling" type="text" placeholder="Country" required disabled>
                 </div>
             </div>
             <div class="field">
                 <label class="label">State</label>
                 <div class="control">
-                    <input name="state" class="input" type="text" placeholder="State" required>
+                    <input name="state" class="input disabling" type="text" placeholder="State" required disabled>
                 </div>
             </div>
         </div>
@@ -64,13 +64,13 @@
             <div class="field">
                 <label class="label">Postal Code</label>
                 <div class="control">
-                    <input name="postalcode" class="input" type="text" placeholder="Postal Code" required>
+                    <input name="postalcode" class="input disabling" type="text" placeholder="Postal Code" required disabled>
                 </div>
             </div>
             <div class="field">
                 <label class="label">City</label>
                 <div class="control">
-                    <input name="city" class="input" type="text" placeholder="City" required>
+                    <input name="city" class="input disabling" type="text" placeholder="City" required disabled>
                 </div>
             </div>
         </div>
@@ -78,13 +78,13 @@
             <div class="field">
                 <label class="label">Street</label>
                 <div class="control">
-                    <input name="street" class="input" type="text" placeholder="Street" required>
+                    <input name="street" class="input disabling" type="text" placeholder="Street" required disabled>
                 </div>
             </div>
             <div class="field">
                 <label class="label">Street Num.</label>
                 <div class="control">
-                    <input name="streetnumber" class="input" type="text" placeholder="Street Number" required>
+                    <input name="streetnumber" class="input disabling" type="text" placeholder="Street Number" required disabled>
                 </div>
             </div>
         </div>
@@ -92,7 +92,7 @@
             <div class="field">
                 <label class="label">Highest Degree</label>
                 <div class="select">
-                    <select name="degree" required>
+                    <select class="disabling" name="degree" required disabled>
                         <option value="highest">Highest Degree</option>
                     </select>
                 </div>
@@ -103,7 +103,7 @@
             <div class="field">
                 <label class="label">Industry</label>
                 <div class="select">
-                    <select name="industry" required>
+                    <select class="disabling" name="industry" required disabled>
                         <option value="industry">Industry</option>
                     </select>
                 </div>
@@ -114,7 +114,7 @@
             <div class="field">
                 <label class="label">Allow Headhunting</label>
                 <label class="checkbox">
-                    <input class="checkbox-input" name="headhunting" type="checkbox">
+                    <input class="checkbox-input disabling" name="headhunting" type="checkbox" disabled>
                     Yes
                 </label>
             </div>  
@@ -136,19 +136,19 @@
                             <th>1</th>
                             <td>Lebenslauf</td>
                             <td>.docx</td>
-                            <td><button type="button">delete</button></td>
+                            <td><button class="disabling" type="button" disabled>delete</button></td>
                         </tr>
                         <tr>
                             <th>2</th>
                             <td>LAP Zeugnis</td>
                             <td>.pdf</td>
-                            <td><button type="button">delete</button></td>
+                            <td><button class="disabling" type="button" disabled>delete</button></td>
                         </tr>
                         <tr>
                             <th>3</th>
                             <td>Matura Zeugnis</td>
                             <td>.png</td>
-                            <td><button type="button">delete</button></td>
+                            <td><button class="disabling" type="button" disabled>delete</button></td>
                         </tr>
                     </tbody>
                 </table>
@@ -158,7 +158,7 @@
             <div class="field">
                 <div class="file">
                     <label class="file-label">
-                        <input class="file-input" type="file" name="file">
+                        <input class="file-input disabling" type="file" name="file" disabled>
                         <span class="file-cta">
                             <span class="file-icon">
                                 <i class="fas fa-upload"></i>
@@ -170,9 +170,15 @@
             </div>
             <div class="field"></div>
         </div>
-        <diV class="row">
-            <button class="button is-link" disabled>Change</button>
+        <diV class="row edit">
+            <button type="button" class="button is-link" onclick="edit()">Edit Profile</button>
+        </diV>
+        <diV class="row cancel hide">
+            <button type="button" class="button" onclick="cancel()">Cancel</button>
+            <button class="button is-link">Change</button>
         </diV>
     </form>
 </body>
 </html>
+
+<script src="../../scripts/hideButton.js"></script>
