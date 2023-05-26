7a95fdd8aeb84989ccaf43dccd78ca7d257921b2
lenapopena <lena.wurmsdobler@gmail.com>
2023-05-26 11:11:19 +0200

    added navbar call to files

--- source/view/company/company_overview.php
+++ source/view/company/company_overview.php
@@ -1,4 +1,27 @@
 <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Overview</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="https://bulma.io/vendor/fontawesome-free-5.15.2-web/css/all.min.css">
+</head>
+<body>
+<?php require_once 'company_navbar.php'; ?>
+<section class="section">
+    <div class="container">
+        <h1 class="title">
+            Hello World
+        </h1>
+        <p class="subtitle">
+            Company_overview.php
+        </p>
+    </div>
+</section>
+</body>
+</html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
