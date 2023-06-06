7d9b3584868e02fa43e4ecda4ac7cb1892e8a3ca
Kaiser-exe <david.kaiser03@gmail.com>
2023-06-06 11:22:22 +0200

    finished applicant overview

--- website/applicant/pages/applicant_overview.php
+++ website/applicant/pages/applicant_overview.php
@@ -1,23 +1,143 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Overview</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="../../source/css/bulma.css">
+    <link rel="stylesheet" href="../../source/css/applicant.css">
     <link rel="stylesheet" href="https://bulma.io/vendor/fontawesome-free-5.15.2-web/css/all.min.css">
+    <script src="../../source/js/favourites.js"></script>
 </head>
 <body>
-<?php require_once 'applicant_navbar.php'; ?>
-<section class="section">
-    <div class="container">
-        <h1 class="title">
-            Hello World
-        </h1>
-        <p class="subtitle">
-            Applicant_overview.php
-        </p>
+<?php require_once '../parts/applicant_profile_navbar.php'; ?>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        4 jobs currently available
+                    </p>
+                </header>
+            </div>
+        </div>
     </div>
-</section>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Jobtitle
+                        <div class="like">
+                            <span class="icon is-small">
+                                <i class="fas fa-heart liked"></i>
+                            </span>
+                        </div>
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <div>
+                            Some Content. Don't know which content!
+                        </div>
+                        <div>
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-info"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+            <header class="card-header">
+                    <p class="card-header-title">
+                        Jobtitle
+                        <div class="like">
+                            <span class="icon is-small">
+                                <i class="fas fa-heart"></i>
+                            </span>
+                        </div>
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <div>
+                            Some Content. Don't know which content!
+                        </div>
+                        <div>
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-info"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+            <header class="card-header">
+                    <p class="card-header-title">
+                        Jobtitle
+                        <div class="like">
+                            <span class="icon is-small">
+                                <i class="fas fa-heart"></i>
+                            </span>
+                        </div>
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <div>
+                            Some Content. Don't know which content!
+                        </div>
+                        <div>
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-info"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+            <header class="card-header">
+                    <p class="card-header-title">
+                        Jobtitle
+                        <div class="like">
+                            <span class="icon is-small">
+                                <i class="fas fa-heart"></i>
+                            </span>
+                        </div>
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <div>
+                            Some Content. Don't know which content!
+                        </div>
+                        <div>
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-info"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
