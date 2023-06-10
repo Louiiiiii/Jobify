effc00deb512a2fa592ed7750d0d8776bab2680c
Kaiser-exe <david.kaiser03@gmail.com>
2023-06-10 17:21:05 +0200

    created company published jobs

---
+++ website/company/pages/company_published.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overview</title>
+    <link rel="stylesheet" href="../../source/css/bulma.css">
+    <link rel="stylesheet" href="../../source/css/applicant.css">
+    <link rel="stylesheet" href="https://bulma.io/vendor/fontawesome-free-5.15.2-web/css/all.min.css">
+    <script src="../../source/js/favourites.js"></script>
+</head>
+<body>
+<?php require_once '../parts/company_profile_navbar.php'; ?>
+    <div class="search-row">
+        <div class="field">
+            <p class="control has-icons-left">
+                <input class="input" placeholder="Search">
+                <span class="icon is-small is-left">
+                    <i class="fas fa-search"></i>
+                </span>
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Jobtitle
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <div>
+                            Some Content. Don't know which content!
+                        </div>
+                        <div class="content__button">
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Jobtitle
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <div>
+                            Some Content. Don't know which content!
+                        </div>
+                        <div class="content__button">
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+    <div class="column">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Jobtitle
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <div>
+                            Some Content. Don't know which content!
+                        </div>
+                        <div class="content__button">
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Jobtitle
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <div>
+                            Some Content. Don't know which content!
+                        </div>
+                        <div class="content__button">
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="add-row">
+        <button class="button is-info is-rounded">
+            <span class="icon is-small">
+                <i class="fas fa-plus"></i>
+            </span>
+        </button>
+    </div>
+</body>
+</html>
