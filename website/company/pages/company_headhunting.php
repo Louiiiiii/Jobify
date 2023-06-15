a459cffedeb04a59d7554620f4036c67fd2aeffb
Kaiser-exe <david.kaiser03@gmail.com>
2023-06-15 08:05:32 +0200

    designed company_headhunting

---
+++ website/company/pages/company_headhunting.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overview</title>
+    <link rel="stylesheet" href="../../source/css/bulma.css">
+    <link rel="stylesheet" href="../../source/css/applicant.css">
+    <link rel="stylesheet" href="../../source/css/headhunting.css">
+    <link rel="stylesheet" href="https://bulma.io/vendor/fontawesome-free-5.15.2-web/css/all.min.css">
+    <script src="../../source/js/favourites.js"></script>
+</head>
+<body>
+<?php require_once '../parts/company_profile_navbar.php'; ?>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+                        <div class="left-content">
+                            <div>Human 1</div>
+                            <figure class="image is-64x64">
+                                <img class="is-rounded" src="../../source/img/user-icon.png">
+                            </figure>
+                        </div>
+                        <div class="content__button">
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-envelope-open-text"></i>
+                                </span>
+                            </button>
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-info"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+                        <div class="left-content">
+                            <div>Human 2</div>
+                            <figure class="image is-64x64">
+                                <img class="is-rounded" src="../../source/img/user-icon.png">
+                            </figure>
+                        </div>
+                        <div class="content__button">
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-envelope-open-text"></i>
+                                </span>
+                            </button>
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-info"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+    <div class="column">
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+                        <div class="left-content">
+                            <div>Human 3</div>
+                            <figure class="image is-64x64">
+                                <img class="is-rounded" src="../../source/img/user-icon.png">
+                            </figure>
+                        </div>
+                        <div class="content__button">
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-envelope-open-text"></i>
+                                </span>
+                            </button>
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-info"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+                        <div class="left-content">
+                            <div>Human 4</div>
+                            <figure class="image is-64x64">
+                                <img class="is-rounded" src="../../source/img/user-icon.png">
+                            </figure>
+                        </div>
+                        <div class="content__button">
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-envelope-open-text"></i>
+                                </span>
+                            </button>
+                            <button class="button is-info is-rounded">
+                                <span class="icon is-small">
+                                    <i class="fas fa-info"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
