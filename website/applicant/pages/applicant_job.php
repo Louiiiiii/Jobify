b2f48aaa8e20b1134e4367af85cc3bbdd69eed31
Louiiiiii <leon.oberndorfer@gmail.com>
2023-06-15 08:56:07 +0200

    added applicant_job

--- website/applicant/pages/applicant_job.php
+++ website/applicant/pages/applicant_job.php
@@ -15,125 +15,43 @@
     <div class="row">
         <div class="column">
             <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title">
-                        4 jobs currently available
-                    </p>
-                </header>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title">
-                        Jobtitle
-                        <div class="like">
-                            <span class="icon is-small">
-                                <i class="fas fa-heart liked"></i>
-                            </span>
-                        </div>
-                    </p>
-                </header>
-                <div class="card-content">
-                    <div class="content">
-                        <div>
-                            Some Content. Don't know which content!
-                        </div>
-                        <div>
-                            <button class="button is-info is-rounded">
-                                <span class="icon is-small">
-                                    <i class="fas fa-info"></i>
-                                </span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="column">
-            <div class="card">
             <header class="card-header">
                     <p class="card-header-title">
-                        Jobtitle
-                        <div class="like">
-                            <span class="icon is-small">
-                                <i class="fas fa-heart"></i>
-                            </span>
-                        </div>
+                        Job as Gooflord
                     </p>
                 </header>
                 <div class="card-content">
                     <div class="content">
-                        <div>
-                            Some Content. Don't know which content!
+                        <div class="header">
+                            <b><h6>Goofy Gmbh Marchtrenk;    we make a difference</h6></b>
                         </div>
                         <div>
-                            <button class="button is-info is-rounded">
-                                <span class="icon is-small">
-                                    <i class="fas fa-info"></i>
-                                </span>
-                            </button>
+                          Wir suchen einen motivierten und goofy Junior Marketing Manager, der Teil unseres dynamischen und goofy Teams werden möchte. In dieser goofy Rolle unterstützen Sie uns bei der Entwicklung und Umsetzung von goofy Marketingstrategien, der goofy Marktforschung und der Erstellung von goofy Marketingmaterialien. Sie arbeiten goofy eng mit dem Marketingteam zusammen, um goofy innovative Kampagnen zu entwickeln und unsere goofy Marktposition zu stärken. Der goofy ideale Kandidat verfügt über eine goofy Leidenschaft für Marketing, eine goofy kreative Denkweise und goofy gute kommunikative Fähigkeiten. Erfahrungen im goofy digitalen Marketing und mit goofy Social-Media-Kanälen sind von goofy Vorteil. Wenn Sie eine goofy Karriere im Marketing anstreben und goofy bereit sind, neue goofy Herausforderungen anzunehmen, freuen wir uns auf Ihre goofy Bewerbung.
+                        </div>
+                        <div>
+                          <br><b>Anforderungen:</b>
+                          <ul>
+                            <li>Fachkenntnisse</li>
+                            <li>Erfahrung</li>
+                            <li>Kreativität</li>
+                            <li>Zuverlässigkeit</li>
+                          </ul>
+                        </div>
+                        <div>
+                          <br><b>Benefits:</b>
+                          <ul>
+                            <li>Flexible Arbeitszeiten</li>
+                            <li>Mitarbeiterbeteiligung</li>
+                            <li>Gesundheitsförderung</li>
+                            <li>Teamarbeit und Zusammenhalt</li>
+                          </ul>
+                        </div>
+                        <div>
+                          <br><b>Gehalt: 3200€ brutto</b>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-            <header class="card-header">
-                    <p class="card-header-title">
-                        Jobtitle
-                        <div class="like">
-                            <span class="icon is-small">
-                                <i class="fas fa-heart"></i>
-                            </span>
-                        </div>
-                    </p>
-                </header>
-                <div class="card-content">
-                    <div class="content">
-                        <div>
-                            Some Content. Don't know which content!
-                        </div>
-                        <div>
-                            <button class="button is-info is-rounded">
-                                <span class="icon is-small">
-                                    <i class="fas fa-info"></i>
-                                </span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="column">
-            <div class="card">
-            <header class="card-header">
-                    <p class="card-header-title">
-                        Jobtitle
-                        <div class="like">
-                            <span class="icon is-small">
-                                <i class="fas fa-heart"></i>
-                            </span>
-                        </div>
-                    </p>
-                </header>
-                <div class="card-content">
-                    <div class="content">
-                        <div>
-                            Some Content. Don't know which content!
-                        </div>
-                        <div>
-                            <button class="button is-info is-rounded">
-                                <span class="icon is-small">
-                                    <i class="fas fa-info"></i>
-                                </span>
-                            </button>
-                        </div>
+                    <div>
+                        <button class="button is-black">Apply</button>
                     </div>
                 </div>
             </div>
